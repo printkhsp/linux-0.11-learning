2f2d73e85b28f106e3593c3ce3e8852df7be3291
hsp <2397070077@qq.com>
2017-02-08 10:38:57 +0800

    编译中场提交

--- fs/exec.c
+++ fs/exec.c
@@ -135,10 +135,11 @@ static unsigned long copy_strings(int argc,char ** argv,unsigned long *page,
 				offset = p % PAGE_SIZE;
 				if (from_kmem==2)
 					set_fs(old_fs);
-				if (!(pag = (char *) page[p/PAGE_SIZE]) &&
-				    !(pag = (char *) page[p/PAGE_SIZE] =
-				      (unsigned long *) get_free_page())) 
+				if (!(pag = (char *) page[p/PAGE_SIZE])) {
+					page[p/PAGE_SIZE] = (unsigned long *) get_free_page(); 
+				    if (!(pag = (char *) page[p/PAGE_SIZE]))
 					return 0;
+				}
 				if (from_kmem==2)
 					set_fs(new_fs);
 
